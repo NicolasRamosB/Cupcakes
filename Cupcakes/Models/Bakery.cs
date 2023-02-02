502624ef0255d102878ffc41de3dfc1f64832fb2
NicolasRamos <nicolasramos.borner@gmail.com>
2023-02-02 08:50:55 -0300

    Agregar archivos de proyecto.

---
+++ Cupcakes/Models/Bakery.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cupcakes.Models
+{
+    public class Bakery
+    {
+        [Key]
+        public int BakeryId { get; set; }
+
+        [StringLength(50,MinimumLength =4, ErrorMessage = "El rango es de 4 a 50 caracteres")]
+        public string BakeryName { get; set; }
+
+        [Range(1,40,ErrorMessage ="El rango es de 1 a 40")]
+        public int Quantity { get; set; }
+
+        [StringLength(50,MinimumLength =4,ErrorMessage ="El rango es de 4 a 50 caracteres")]
+        public string Address { get; set; }
+
+        public virtual List<Cupcake> Cupcakes { get; set; }
+
+    }       
+}
---
+++ Cupcakes/Repositories/ICupcakeRepository.cs
@@ -0,0 +1,20 @@
+﻿using Cupcakes.Data;
+using Cupcakes.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cupcakes.Repositories
+{
+    public interface ICupcakeRepository
+    {
+
+        IEnumerable<Cupcake> GetCupcakes();
+        Cupcake GetCupcakeById(int id);
+        void CreateCupcake(Cupcake cupcake);
+        void DeleteCupcake(int id);
+        void Savechanges();
+        IQueryable<Bakery> PopulateBakeriesDropDownList();
+        
+
+    }
+}
